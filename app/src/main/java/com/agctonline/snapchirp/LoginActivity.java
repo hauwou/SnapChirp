36386a7df2c6fc6dab362eadbaa3099d9c1e7098
hauwou <hauwou@gmail.com>
2014-11-24 20:51:47 -0800

    Initial commit

---
+++ app/src/main/java/com/agctonline/snapchirp/LoginActivity.java
@@ -0,0 +1,44 @@
+package com.agctonline.snapchirp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+/*App similar to snapchat
+
+        Features:
+
+        Use camera to capture images and videos Send files to users Receive files from users Use cloud services to store data Add friends Delete friends*/
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_login, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
