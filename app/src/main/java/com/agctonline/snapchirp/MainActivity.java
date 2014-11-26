4eaa7a7552a16332034b9727bf4ed14673b1009b
hauwou <hauwou@gmail.com>
2014-11-25 18:15:02 -0800

    Created multiple activities and UP button, and clear task

--- app/src/main/java/com/agctonline/snapchirp/MainActivity.java
+++ app/src/main/java/com/agctonline/snapchirp/MainActivity.java
@@ -7,6 +7,7 @@ import android.app.ActionBar;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.support.v13.app.FragmentPagerAdapter;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
@@ -40,6 +41,16 @@ public class MainActivity extends Activity implements ActionBar.TabListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent loginIntent = new Intent(this, LoginActivity.class);
+
+        loginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        loginIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        //New_Task declares that login Activity is a new task
+        //Clear_task clears the Main activity task
+        //the goal of this is to not have the back button redirect to the MainActivity
+
+        startActivity(loginIntent);
 
         // Set up the action bar.
         final ActionBar actionBar = getActionBar();
