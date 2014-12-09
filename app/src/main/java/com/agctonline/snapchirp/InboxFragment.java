889d836df32abe37c789c595311846b9aac715e3
hauwou <hauwou@gmail.com>
2014-12-08 20:22:11 -0800

    Added view image and view video messages

--- app/src/main/java/com/agctonline/snapchirp/InboxFragment.java
+++ app/src/main/java/com/agctonline/snapchirp/InboxFragment.java
@@ -1,6 +1,8 @@
 package com.agctonline.snapchirp;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import android.support.v4.app.ListFragment;
@@ -8,10 +10,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
@@ -81,6 +85,31 @@ public class InboxFragment extends ListFragment {
         });
 
 
+
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        ParseObject message = mMessages.get(position);
+        String messageType = message.getString(ParseConstants.KEY_FILE_TYPE);
+
+        ParseFile file = message.getParseFile(ParseConstants.KEY_FILE);
+        Uri fileUri = Uri.parse(file.getUrl());
+
+        if (messageType.equals("photo")){
+            //view image
+            Intent gotoViewImageActivity = new Intent(getActivity(), ViewImageActivity.class);
+            gotoViewImageActivity.setData(fileUri);
+            startActivity(gotoViewImageActivity);
+        }
+        else {
+            //view video
+            Intent gotoViewVideo = new Intent(Intent.ACTION_VIEW, fileUri);
+            gotoViewVideo.setDataAndType(fileUri,"video/*");
+            startActivity(gotoViewVideo);
+        }
 
     }
 }
--- app/src/main/java/com/agctonline/snapchirp/ViewImageActivity.java
+++ app/src/main/java/com/agctonline/snapchirp/ViewImageActivity.java
@@ -1,9 +1,13 @@
 package com.agctonline.snapchirp;
 
 import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 
 public class ViewImageActivity extends Activity {
@@ -12,6 +16,13 @@ public class ViewImageActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_image);
+
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        Uri imageUri = getIntent().getData();
+        //Use picasso library to load web image into imageview
+        Picasso.with(this).load(imageUri.toString()).into(imageView);
+
+
     }
 
 
